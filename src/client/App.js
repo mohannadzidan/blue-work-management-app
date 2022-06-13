88874c2ea9180d1d95b8b6da2de16d96da199dfb
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-14 01:04:27 +0200

    feat(client): add sign-in and sign-up templates

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react';
-import './app.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import ReactImage from './react.png';
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 
-export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
+function Home() {
+  const [user, setUser] = useState(null);
+  const [redirect, setRedirect] = useState(undefined);
+  const nav = useNavigate();
+  useEffect(() => {
+    fetch('/api/me')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+      .then(json => {
+        if (json.status) {
+          setUser(user)
+        } else {
+          nav('/sign-in');
+        }
+      }).catch(console.error)
+  }, []);
+  return <div>
+    {user ? <h1>{`Hello`}</h1> : <h1>Loading.. please wait!</h1>}
+    <img src={ReactImage} alt="react" />
+  </div>
 }
+
+
+export default function App({ }) {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+

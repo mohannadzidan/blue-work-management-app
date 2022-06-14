3c72e8a5b8e6fdba9bb5228ceb0e49d3c4407ab9
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-14 05:55:12 +0200

    feat(client): add onClick to AddTodoButton

--- src/client/components/list/AddTodoButton.js
+++ src/client/components/list/AddTodoButton.js
@@ -9,9 +9,9 @@ import React, { useEffect, useState } from 'react';
 import { Plus } from "react-bootstrap-icons";
 
 
-export default function AddTodoButton({ }) {
+export default function AddTodoButton({ onClick }) {
 
-    return (<div className="w-100 clickable text-muted d-flex align-items-center">
+    return (<div className="w-100 clickable text-muted d-flex align-items-center mt-2" onClick={onClick}>
         <Plus size={24} />
         <span>add task</span>
     </div>)

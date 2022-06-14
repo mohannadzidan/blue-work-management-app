baad9bc164cccf06e6faf9b7530fab5b3caff9d4
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-14 05:51:43 +0200

    fix(client): remove unused code

--- src/client/components/list/List.js
+++ src/client/components/list/List.js
@@ -1,26 +1,20 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link,
-    useNavigate
-} from "react-router-dom";
 import React, { useEffect, useState } from 'react';
 import './List.css';
 import AddTodoButton from "./AddTodoButton";
 import Todo from "../todo/Todo";
-const longDescription = `Legacy versions of mkdirp are no longer supported. Please update to mkdirp 1.x. (Note that the API surface has changed to use Promises in 1.x.)
-warning heroku-repo > @heroku-cli/plugin-run > @heroku-cli/command > cli-ux@4.9.3: Package no longer supported. Contact Support at https://www.npmjs.com/support for more info.
-Installing plugin heroku-repo... installed v1.0.14`
+import AddTaskModal from '../modals/AddTaskModal';
+
 
 export default function List({ title, todos }) {
+    const [isAddTaskModalOpen, setAddTaskModalOpen] = React.useState(false);
 
-    return (<div className="p-2 me-3 blue-list" >
+    return (<div className="p-2 me-3 blue-list flex-shrink-0 d-flex flex-column" >
         <div className="list-title">{title}</div>
         <div className="my-1">
             {todos.map((todo, i) => <Todo key={i} {...todo}></Todo>)}
         </div>
-        <AddTodoButton />
+        <AddTodoButton onClick={() => setAddTaskModalOpen(true)}/>
+        <AddTaskModal listId={title} listTitle={title} onClose={() => setAddTaskModalOpen(false)} isOpen={isAddTaskModalOpen} />
     </div>)
 }
 

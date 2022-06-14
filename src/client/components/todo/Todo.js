5ff57613df4df8234e285df2f4b94ba224e32647
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-14 05:50:17 +0200

    feat(client): add checkbox to Todo component

--- src/client/components/todo/Todo.js
+++ src/client/components/todo/Todo.js
@@ -11,10 +11,13 @@ import { ThreeDotsVertical } from "react-bootstrap-icons";
 
 
 export default function Todo({ startDate, endDate, priority, status, title, description }) {
+    const [isFinished, setFinished] = useState(status);
     return (<div className="clickable todo-item p-2">
         <div className="d-flex align-items-center">
             <span className="todo-title flex-grow-1">{title}</span>
-            <ThreeDotsVertical className="clickable" size={24}/>
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+            </div>
         </div>
     </div>)
 }

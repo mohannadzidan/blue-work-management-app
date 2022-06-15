ceb10fadc6f151bd81d022ba11a48e6052e76b43
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-15 22:18:20 +0200

    build: update packages to latest update

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
+import * as ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(<App />);

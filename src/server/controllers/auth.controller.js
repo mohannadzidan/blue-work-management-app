629c90811325a26d861f160fda7e2abeb8d513fd
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-17 01:19:25 +0200

    feat(server): add reset password and verification endpoints

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const { successResponse, failResponse, safeUser } = require("../helpers/methods"
 const jwt = require('jsonwebtoken');
 const { User } = require("../db");
 const bcrypt = require('bcrypt');
-
+const { sendConfirmationEmail, sendResetPasswordEmail } = require('../emails');
 const SALT = parseInt(process.env.SALT)
 /**
  *
@@ -18,23 +18,111 @@ exports.signUpEmailAndPassword = async (req, res) => {
             res.status(409).json(failResponse('EMAIL_ALREADY_EXISTS'));
             return;
         }
+        // verification token lives for an hour
+        const verificationToken = jwt.sign({ email: email }, process.env.SECRET, { issuer: 'blue-work-management-app.com', subject: 'email_verification_token', expiresIn: 3600 });
+        const verificationUrl = process.env.APP_URL + '/verification?token=' + verificationToken;
+        console.log('verificationUrl', verificationUrl);
+        const confirmationMail = await sendConfirmationEmail(email, verificationUrl);
+        if (confirmationMail.status !== 200) throw confirmMailStatus;
         const hashedPassword = await bcrypt.hash(password, SALT);
-
-        const user = await new User({
+        await new User({
             firstName: firstName,
             lastName: lastName,
             email: email,
-            password: hashedPassword
+            password: hashedPassword,
+            emailVerified: false,
         }).save();
-        const accessToken = jwt.sign({ id: user._id }, process.env.SECRET, { issuer: 'blue-work-management-app.com', subject: 'access_token', expiresIn: 86400 });
-        res.status(201)
-            .cookie('access_token', accessToken, { httpOnly: true, expires: new Date(Date.now() + 86400000) })
-            .json(successResponse(safeUser(user)));
+        res.status(201).json(successResponse({
+            email: email
+        }));
     } catch (e) {
-        res.json(failResponse('INTERNAL_ERROR'));
+        res.status(500).json(failResponse('INTERNAL_ERROR'));
         console.error(e);
     }
 
+}
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @returns 
+ */
+exports.requestPasswordReset = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const userWithSameEmail = await User.findOne({ email: email })
+        if (!userWithSameEmail) {
+            return res.status(404).json(failResponse('USER_NOT_FOUND'));
+        }
+        // password reset token lives for 15 minutes
+        const passwordResetToken = jwt.sign({ email: email }, process.env.SECRET, { issuer: 'blue-work-management-app.com', subject: 'password_reset_token', expiresIn: 900 });
+        const passwordResetUrl = process.env.APP_URL + '/reset-password?token=' + passwordResetToken;
+        console.log('passwordResetUrl', passwordResetUrl);
+        const passwordResetMail = await sendResetPasswordEmail(email, passwordResetUrl);
+        if (passwordResetMail.status !== 200) throw new Error(passwordResetMail);
+        res.status(201).json(successResponse({ email: email }));
+    } catch (e) {
+        res.status(500).json(failResponse('INTERNAL_ERROR'));
+        console.error(e);
+    }
+}
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @returns 
+ */
+exports.validatePasswordResetToken = async (req, res) => {
+    try {
+        const { token: tokenString } = req.body;
+        try {
+            var token = jwt.verify(tokenString, process.env.SECRET);
+            if (token.sub !== 'password_reset_token') throw new Error();
+            res.status(201).json(successResponse({
+                email: token.email
+            }));
+        } catch {
+            return res.status(400).json(failResponse('INVALID_TOKEN'));
+        }
+    } catch (e) {
+        res.status(500).json(failResponse('INTERNAL_ERROR'));
+        console.error(e);
+    }
+}
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @returns 
+ */
+exports.resetPassword = async (req, res) => {
+    try {
+        const { token: tokenString, newPassword } = req.body;
+        try {
+            var token = jwt.verify(tokenString, process.env.SECRET);
+            if (token.sub !== 'password_reset_token') throw new Error();
+            res.status(201).json(successResponse({
+                email: token.email
+            }));
+        } catch {
+            return res.status(400).json(failResponse('INVALID_TOKEN'));
+        }
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(404).json(failResponse('USER_NOT_FOUND'));
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, SALT);
+        user.password = hashedPassword;
+        await user.save();
+        res.status(201).json(successResponse({
+            email: token.email
+        }));
+    } catch (e) {
+        res.status(500).json(failResponse('INTERNAL_ERROR'));
+        console.error(e);
+    }
 }
 
 /**
@@ -53,12 +141,43 @@ exports.signInEmailAndPassword = async (req, res) => {
             const accessToken = jwt.sign({ id: user._id }, process.env.SECRET, { issuer: 'blue-work-management-app.com', subject: 'access_token', expiresIn: 86400 });
             res.cookie('access_token', accessToken, { httpOnly: true, expires: new Date(Date.now() + 86400000) }).json(successResponse(safeUser(user)))
         } else if (isPasswordOk) {
-            res.json(failResponse('EMAIL_NOT_VERIFIED'));
+            res.status(403).json(failResponse('EMAIL_NOT_VERIFIED'));
         } else {
             res.status(401).json(failResponse('INCORRECT_EMAIL_OR_PASSWORD'));
         }
     } catch (e) {
         res.status(500).json(failResponse('INTERNAL_ERROR'));
+        console.error(e);
+    }
+}
+
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @returns 
+ */
+exports.verifyUserEmail = async (req, res) => {
+    try {
+        const { token: tokenString } = req.body;
+        try {
+            var token = jwt.verify(tokenString, process.env.SECRET);
+            if (token.sub !== 'email_verification_token') throw new Error();
+        } catch {
+            return res.status(400).json(failResponse('INVALID_TOKEN'));
+        }
+        const user = await User.findOne({ email: token.email });
+        if (!user) {
+            return res.status(404).json(failResponse('USER_NOT_EXIST'));
+        }
+        user.emailVerified = true;
+        await user.save();
+        res.status(201).json(successResponse({
+            email: token.email
+        }));
+    } catch (e) {
+        res.json(failResponse('INTERNAL_ERROR'));
         console.error(e);
     }
 }
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -10,25 +10,42 @@ const auth = require('../controllers/auth.controller');
 const users = require('../controllers/users.controller');
 const { getTask, updateTask: updateTask, getAllTasks, deleteTask, createTask } = require("../controllers/tasks.controller");
 
-
+// /api/auth
 router.post('/api/auth/signUpEmailAndPassword', validate([
-    body('email').isString().isLength({ min: 6, max: 32 }),
+    body('email').isEmail(),
     body('password').isString().isLength({ min: 8, max: 24 }),
-    body('firstName').isString().isLength({ min: 1, max: 24 }),
-    body('lastName').isString().isLength({ min: 1, max: 24 }),
+    body('firstName').trim().isString().isLength({ min: 1, max: 24 }),
+    body('lastName').trim().isString().isLength({ min: 1, max: 24 }),
 ]), auth.signUpEmailAndPassword);
 
 router.post('/api/auth/signInEmailAndPassword', validate([
-    body('email').isString().isLength({ min: 6, max: 32 }),
+    body('email').isEmail(),
     body('password').isString().isLength({ min: 8, max: 24 }),
 ]), auth.signInEmailAndPassword);
 
+router.post('/api/auth/verifyUserEmail', validate([
+    body('token').isString(),
+]), auth.verifyUserEmail);
+
+router.post('/api/auth/requestPasswordReset', validate([
+    body('email').isEmail(),
+]), auth.requestPasswordReset);
+
+router.post('/api/auth/validatePasswordResetToken', validate([
+    body('token').isString(),
+]), auth.validatePasswordResetToken);
+
+router.post('/api/auth/resetPassword', validate([
+    body('token').isString(),
+    body('newPassword').isEmail(),
+]), auth.resetPassword);
+
 router.post('/api/auth/signOut', authenticate, auth.signOut);
 
-
+// /api/users
 router.get('/api/users/me', authenticate, users.me);
 
-
+// /api/tasks
 router.get('/api/tasks', authenticate, getAllTasks);
 router.get('/api/tasks/:taskId', validate([
     param('taskId').exists()

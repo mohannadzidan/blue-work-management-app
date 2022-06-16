ac82e7d1eb9f060fc392caf94fa5f53b1c44a6eb
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-16 19:51:46 +0200

    refactor(server): change server starting message

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,5 @@ require("dotenv").config()
 const app = require("./server")
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`app listening at port http://localhost:${process.env.PORT || 8080}`)
+    console.log(`app started listening at port ${process.env.PORT || 8080}`)
 })

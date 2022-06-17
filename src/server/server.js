7951a64dbd9fa6ae2f5acda386e37d6c9fd5134d
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-17 02:52:57 +0200

    fix(server): path for static files

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(cookieParser());
 app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
-app.use(express.static('../dist'));
+app.use(express.static('dist'));
 app.use(api);
 
 module.exports = app

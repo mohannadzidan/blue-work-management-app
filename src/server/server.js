3e017a3ce4531ef9c2da432901b48f31fba88590
Mohannad Zidan <moh.anad23823@gmail.com>
2022-06-17 03:11:24 +0200

    feat(server): redirect requests to index.html

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,14 @@ const cookieParser = require("cookie-parser");
 const express = require("express")
 const app = express()
 const cors = require("cors");
+const path = require('path');
 app.use(cors())
 app.use(cookieParser());
 app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 app.use(express.static('dist'));
 app.use(api);
-
+app.use('/*', function (req, res) {
+    res.sendFile(path.resolve('./dist/index.html'));
+})
 module.exports = app
